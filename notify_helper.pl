be6634580de7bf2c4904b72175e09439c5f3354b
Dmitry Lyanguzov <lyanguzov@complete.ru>
2026-02-06 12:08:14 +0300

    Дописан до работоспособной версии

--- notify_helper.pl
+++ notify_helper.pl
@@ -6,10 +6,45 @@
 use strict;
 use warnings;
 use Proc::Daemon;
+use utf8;
+use Encode;
+use DBI;
+use DBD::Pg;
+use DateTime;
+use open qw( :std :encoding(UTF-8) );
+use vars qw($logger $channel $q $dbh $notify $pid $payload $con $mailsend);
 
-use vars qw($logger);
+#### C O N S T A N T S ####
 $logger = "/usr/bin/logger";
-while(<>){
- `$logger -t "sd-notifier" $_`
+$channel = "status_change";
+$mailsend = "/home/hpsupport/git/complete_1/notification.py";
+
+#### CHECKS AND INIT DAEMON ####
+Proc::Daemon::Init;
+
+$dbh = DBI->connect("dbi:Pg:dbname=sddb","reader","canread",
+{ pg_utf8_flag => 1, pg_enable_utf8 => 1, AutoCommit => 1,
+  RaiseError => 1, PrintError => 0,});
+
+# or die $DBI::errstr;
+
+$q = "LISTEN $channel";
+
+#### MAIN LOOP ######
+$con = 1;
+$SIG{TERM} = sub{$con = 0};
+
+$dbh->do($q);
+
+while($con){
+  while($notify = $dbh->pg_notifies){
+   ($channel, $pid, $payload) = @$notify;
+   `$logger -t "sd-notifier[$pid]" $payload`;
+   `$mailsend $payload 2>/dev/null | /usr/sbin/sendmail -t`;
+  }
+  sleep 5;
 }
+
+$dbh->disconnect();
+exit 0;
 
